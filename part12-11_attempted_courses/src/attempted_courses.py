416b5c6f9f3542780f41c1850f5cdeea845c6c25
Teo Georgiev <t.georgiev.g@gmail.com>
2023-06-18 14:09:40 +0300

    Exercise updates

--- part12-11_attempted_courses/src/attempted_courses.py
+++ part12-11_attempted_courses/src/attempted_courses.py
@@ -8,3 +8,29 @@ class CourseAttempt:
         return f"{self.student_name}, grade for the course {self.course_name} {self.grade}"
 
 # Write your solution here
+def names_of_students(attempts: list): 
+    students = map(lambda student: student.student_name, attempts)
+    return students
+
+def course_names(course_names: list): 
+    # # MY SOLUTION
+    courses_final = []
+    courses = sorted(map(lambda course: course.course_name, attempts))
+    [courses_final.append(course) for course in courses if course not in courses_final]
+    return courses_final
+    
+    # # MODEL SOLUTION
+    # names = map(lambda k: k.course_name, course_names)
+    # # remove duplicates by using a set
+    # return sorted(list(set(names)))
+
+def main():        
+    s1 = CourseAttempt("Peter Python", "Introduction to Programming", 3)
+    s2 = CourseAttempt("Olivia C. Objective", "Introduction to Programming", 5)
+    s3 = CourseAttempt("Peter Python", "Advanced Course in Programming", 2)
+
+    for name in course_names([s1, s2, s3]):
+        print(name)
+
+if __name__ == "__main__":
+    main()
