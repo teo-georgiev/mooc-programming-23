206b2f9b1a077aec5c621c7210c00d69347a49f7
Teo Georgiev <t.georgiev.g@gmail.com>
2023-06-20 21:53:47 +0300

    Exercises 3 and 4 from Part 13

--- part13-03_hundred_robots/src/main.py
+++ part13-03_hundred_robots/src/main.py
@@ -1,1 +1,29 @@
 # WRITE YOUR SOLUTION HERE:
+import pygame
+
+pygame.init()
+window = pygame.display.set_mode((640, 480))
+
+robot = pygame.image.load("robot.png")
+
+window.fill((0,0,0))
+
+width = robot.get_width()
+height = robot.get_height()
+
+# MY SOLUTION
+# for i in range(10): 
+#     for j in range(10):
+#         window.blit(robot, (width + j*width*0.8 + i*0.2*width, height + i*height*0.25))
+
+# MODEL SOLUTION
+for i in range(10):
+    for j in range(10):
+        window.blit(robot, (20+10*i+40*j, 100+i*20))
+
+pygame.display.flip()
+
+while True: 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: 
+            exit()
--- part13-04_random_robots/src/main.py
+++ part13-04_random_robots/src/main.py
@@ -1,1 +1,20 @@
 # WRITE YOUR SOLUTION HERE:
+import pygame 
+from random import randint
+
+pygame.init()
+
+window = pygame.display.set_mode((640, 480))
+robot = pygame.image.load("robot.png")
+
+window.fill((0,0,0))
+
+for i in range(0,1000):
+    window.blit(robot, (randint(0, 590), randint(0,380)))
+    
+pygame.display.flip()
+
+while True: 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
