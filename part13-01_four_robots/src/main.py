5f0d6de1d0edbe4acfdcbd41783e65df1c443bcc
Teo Georgiev <t.georgiev.g@gmail.com>
2023-06-20 20:10:54 +0300

    Exercises 1 and 2 from Part 13

--- part13-01_four_robots/src/main.py
+++ part13-01_four_robots/src/main.py
@@ -7,3 +7,26 @@
 
 # WRITE YOUR SOLUTION HERE:
 
+import pygame
+
+pygame.init()
+window = pygame.display.set_mode((640, 480))
+
+robot = pygame.image.load("robot.png")
+
+window.fill((0,0,0))
+
+width = robot.get_width()
+height = robot.get_height()
+
+window.blit(robot, (0,0))
+window.blit(robot, (640 - width, 0))
+window.blit(robot, (0, 480 - height))
+window.blit(robot, (640 - width, 480 - height))
+
+pygame.display.flip()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
--- part13-02_robots_row/src/main.py
+++ part13-02_robots_row/src/main.py
@@ -1,1 +1,22 @@
 # WRITE YOUR SOLUTION HERE:
+import pygame
+
+pygame.init()
+
+window = pygame.display.set_mode((640, 480))
+
+robot = pygame.image.load("robot.png")
+width = robot.get_width()
+height = robot.get_height()
+place = 0
+
+for i in range(10): 
+    window.blit(robot, (place + width, height))
+    place += width
+    
+pygame.display.flip()
+
+while True: 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
