c97c60a7ca2e0797e955146e5ac9f0619ef76d63
Teo Georgiev <t.georgiev.g@gmail.com>
2023-06-28 21:56:24 +0300

    Events, Part 13

--- part13-14_robot_and_mouse/src/main.py
+++ part13-14_robot_and_mouse/src/main.py
@@ -1,1 +1,28 @@
 # WRITE YOUR SOLUTION HERE:
+import pygame 
+
+pygame.init()
+
+width, height = 640, 480
+screen = pygame.display.set_mode((width, height))
+robot = pygame.image.load("robot.png")
+
+robot_x = 0
+robot_y = 0
+
+clock = pygame.time.Clock()
+
+while True: 
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEMOTION: 
+            robot_x = event.pos[0] - robot.get_width() / 2
+            robot_y = event.pos[1] - robot.get_height() / 2
+             
+            screen.fill((0, 0, 0))
+            screen.blit(robot, (robot_x, robot_y))
+            pygame.display.flip()
+        
+        if event.type == pygame.QUIT: 
+            exit()
+    
+    clock.tick(60)
--- part13-15_robot_location/src/main.py
+++ part13-15_robot_location/src/main.py
@@ -1,1 +1,30 @@
 # WRITE YOUR SOLUTION HERE:
+import pygame
+from random import randint
+
+pygame.init()
+
+width, height = 640, 480
+screen = pygame.display.set_mode((width, height))
+robot = pygame.image.load("robot.png")
+
+robot_x = randint(0, width - robot.get_width())
+robot_y = randint(0, width - robot.get_width())
+
+clock = pygame.time.Clock()
+
+while True: 
+    for event in pygame.event.get(): 
+        if event.type == pygame.MOUSEBUTTONDOWN: 
+            if robot_x <= event.pos[0] <= robot_x + robot.get_width() and robot_y <= event.pos[1] <= robot_y + robot.get_height():
+                robot_x = randint(0, width - robot.get_width())
+                robot_y = randint(0, height - robot.get_height())
+            
+        if event.type == pygame.QUIT: 
+            exit()
+            
+    screen.fill((0, 0, 0))
+    screen.blit(robot, (robot_x, robot_y))
+    pygame.display.flip()
+    
+    clock.tick(60)
